b2e8335a8f21532ce9c1a6df76dd18b4f6952510
Wanjohi Wanjiru <WanjohiWanjiru@gmail.com>
2025-09-21 16:09:23 +0300

    alx structure

--- .next/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__b499071a._.js")
-R.m("[project]/Desktop/Month2/alx-project-0x01/alx-project-0x01/pages/_app.tsx [ssr] (ecmascript)")
-module.exports=R.m("[project]/Desktop/Month2/alx-project-0x01/alx-project-0x01/pages/_app.tsx [ssr] (ecmascript)").exports
